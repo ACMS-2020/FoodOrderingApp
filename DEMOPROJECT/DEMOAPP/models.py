2b0da6cc9b56d8df563ccf4d144a9ff8da8776e5
rishikarednam <64738114+rishikarednam@users.noreply.github.com>
2020-05-03 20:18:49 +0530

    Update models.py

--- DEMOPROJECT/DEMOAPP/models.py
+++ DEMOPROJECT/DEMOAPP/models.py
@@ -13,6 +13,50 @@ class Order(models.Model):
     order_date = models.DateTimeField(default=datetime.now())
     delivery_boy_id = models.CharField(max_length=200,default="not_assigned")
     delivered_date = models.DateTimeField(blank=True, null=True)
+    
+    
+class Customer(models.Model):
+	customer_id=models.AutoField(primary_key=True)
+	first_name=models.CharField(max_length=20)
+	last_name=models.CharField(max_length=20)
+	location=models.CharField(max_length=20)
+	phone_number=models.IntegerField()
+	email_id=models.CharField(max_length=30)
+
+def __unicode__(self):
+	return self.first_name
+
+class Delivery_Person(models.Model):
+	delivery_id=models.AutoField(primary_key=True)
+	first_name=models.CharField(max_length=20)
+	last_name=models.CharField(max_length=20)
+	phone_number=models.IntegerField()
+	rating=models.DecimalField(max_digits=2,decimal_places=2, default='0')
+	email_id=models.CharField(max_length=30)
+
+def __str__(self):
+	return self.first_name	
+
+class Restaurant(models.Model):
+	restaurant_id=models.AutoField(primary_key=True)
+	name=models.CharField(max_length=50)
+	phone_number=models.IntegerField()
+	location=models.CharField(max_length=50)
+	RCHOICES=[
+	('One','1'),
+	('Two','2'),
+	('Three','3'),
+	('Four','4'),
+	('Five','5')]
+	rating=models.CharField(default=0, choices=RCHOICES,max_length=10)
+	logo=models.ImageField(upload_to='restaurant_logo/', blank=False)
+
+def __str__(self):
+	return self.name	
+    
+    
+    
+    
 
     # for items"_in_order : Carrot:20,Potato:30"
 
