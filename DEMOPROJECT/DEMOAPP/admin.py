76cd14df7ec1db80c0bfe10c4d519158cee92d71
rishikarednam <64738114+rishikarednam@users.noreply.github.com>
2020-05-03 20:22:30 +0530

    Update admin.py

--- DEMOPROJECT/DEMOAPP/admin.py
+++ DEMOPROJECT/DEMOAPP/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order
+from .models import Order, Customer, Delivery_Person, Restaurant
 
 # Register your models here.
 admin.site.site_header = "Order Database"
@@ -10,3 +10,6 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ['order_id','restaurant_id','user_id','items_in_order','amount','status','order_date','delivery_boy_id','delivered_date']
 
 admin.site.register(Order,OrderAdmin)
+admin.site.register(Customer)
+admin.site.register(Delivery_Person)
+admin.site.register(Restaurant)
