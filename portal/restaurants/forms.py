6cff9395c41e8615f7143151b94bed8fe8f7f53e
sravya-reddy99 <51592413+sravya-reddy99@users.noreply.github.com>
2020-05-15 15:11:14 +0530

    Update forms.py

--- portal/restaurants/forms.py
+++ portal/restaurants/forms.py
@@ -1,7 +1,22 @@
 from django import forms
-from .models import Restaurant
+from .models import Restaurant,FoodItem
 
 class RestaurantForm(forms.ModelForm):
     class Meta:
         model = Restaurant
-        fields = ['id', 'name', 'address', 'cuisines', 'contact_no', 'avg_cost_for_a_person', 'is_delivering_now', 'rating']
+        fields = ['id', 'name', 'image', 'address', 'cuisines', 'contact_no', 'avg_cost_for_a_person', 'is_delivering_now']
+
+class FoodItemsForm(forms.ModelForm):
+    class Meta:
+        model = FoodItem
+        fields = ['food_id','food_name','restaurant_id','price','veg','item_type','cuisine_type','serviceable','image']
+
+class PriceForm(forms.ModelForm):
+    class Meta:
+        model = FoodItem
+        fields = ['food_id','price']
+
+class AcceptableForm(forms.ModelForm):
+    class Meta:
+        model = FoodItem
+        fields = ['food_id','serviceable']
