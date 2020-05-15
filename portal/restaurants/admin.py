259a2e01af24948642fc1a26c84a82f919e5215d
sravya-reddy99 <51592413+sravya-reddy99@users.noreply.github.com>
2020-05-15 15:10:07 +0530

    Update admin.py

--- portal/restaurants/admin.py
+++ portal/restaurants/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import *
+class OrderInline(admin.TabularInline):
+    model = Rating
+    extra=0
+
+
+class FIAdmin(admin.ModelAdmin):
+	list_display = ['food_name','restaurant_id','price','veg','item_type','cuisine_type','serviceable','image']
+	inlines = [OrderInline]
+
+admin.site.register(FoodItem,FIAdmin)
 admin.site.register(Restaurant)
+admin.site.register(OrderUpdate)
+admin.site.register(Order)
+
 
 # Register your models here.
+
